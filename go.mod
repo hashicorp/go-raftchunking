15ebaf08c4dff2db89ce5c2800e8f8354848c26e
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2019-07-17 21:59:33 -0400

    Add test against Raft lib for chunking

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.12
 
 require (
 	github.com/go-test/deep v1.0.2
-	github.com/hashicorp/raft v1.1.1-0.20190717211636-f68fbc6eac5b
+	github.com/hashicorp/raft v1.1.1-0.20190718015810-409e6ad03a3f
 	github.com/kr/pretty v0.1.0
 )
---
+++ raft_test.go
@@ -0,0 +1,82 @@
+package middleware
+
+import (
+	"crypto/rand"
+	"io"
+	"reflect"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/raft"
+)
+
+// This is similar to Raft's TestRaft_TripleNode but performs several large
+// value writes in a row
+func TestRaftStability_Large_Values(t *testing.T) {
+	// Var to store one of the node's FSMs
+	var someFSM *ChunkingFSM
+
+	fsmFunc := func() raft.FSM {
+		ret := NewChunkingFSM(&raft.MockFSM{})
+		if someFSM == nil {
+			someFSM = ret
+		}
+		return ret
+	}
+
+	c := raft.MakeClusterCustomFSM(3, t, raft.DefaultConfig(), fsmFunc)
+	defer c.Close()
+
+	// Should be one leader
+	c.Followers()
+	leader := c.Leader()
+	c.EnsureLeader(t, leader.Leader())
+
+	// Generate a number of large blocks of data
+	var dataBlocks [][]byte
+	for i := 0; i < 50; i++ {
+		data := make([]byte, 10000000)
+		n, err := rand.Read(data)
+		if err != nil && err != io.EOF {
+			t.Fatal(err)
+		}
+		if n != 10000000 {
+			t.Fatalf("expected 10000k bytes to test with, read %d", n)
+		}
+		dataBlocks = append(dataBlocks, data)
+	}
+
+	// Write data blocks in goroutines
+	var wg sync.WaitGroup
+	for _, data := range dataBlocks {
+		// Should be able to apply
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			future := ChunkingApply(data, 5*time.Second, leader.ApplyWithLog)
+			if err := future.Error(); err != nil {
+				c.FailNowf("[ERR] err: %v", err)
+			}
+		}()
+	}
+
+	wg.Wait()
+	c.EnsureSame(t)
+
+	// Verify that each log matches _some_ chunk of data. We've already
+	// verified the FSMs are the same so only need ot look at one
+	mfsm := someFSM.Underlying().(*raft.MockFSM)
+	for _, log := range mfsm.Logs() {
+		var found bool
+		for _, data := range dataBlocks {
+			if reflect.DeepEqual(log, data) {
+				found = true
+				break
+			}
+		}
+		if !found {
+			t.Fatal("did not find a block")
+		}
+	}
+}
