b0f308192e6a5ce8ce029aaa0b210f0e07ff257d
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2019-07-23 18:21:32 -0400

    Allow out-of-order handling
    We now only GC on term change or if we get all chunks.
    
    This also adds CurrentState as a way to get the current state for
    snapshotting.

--- fsm.go
+++ fsm.go
@@ -1,39 +1,31 @@
 package raftchunking
 
 import (
-	"errors"
 	"io"
 
 	"github.com/golang/protobuf/proto"
 	"github.com/hashicorp/errwrap"
 	"github.com/hashicorp/go-raftchunking/types"
 	"github.com/hashicorp/raft"
+	"github.com/mitchellh/copystructure"
 )
-
-var (
-	ErrTermMismatch = errors.New("term mismatch during reconstruction of chunks, please resubmit")
-
-	ErrInvalidOpNum = errors.New("no op num found when reconstructing chunks")
-
-	ErrNoExistingChunks = errors.New("no existing chunks but non-zero sequence num")
-
-	ErrSequenceNumberMismatch = errors.New("sequence number skipped")
-
-	ErrMissingChunk = errors.New("missing sequence number during reconstruction")
-)
-
-type chunkInfo struct {
-	term   uint64
-	seqNum uint32
-	data   []byte
-}
 
 var _ raft.FSM = (*ChunkingFSM)(nil)
 var _ raft.ConfigurationStore = (*ChunkingConfigurationStore)(nil)
 
+// ChunkInfo holds chunk information
+type ChunkInfo struct {
+	Data []byte
+}
+
+// ChunkMap represents a set of data chunks. We use ChunkInfo with Data instead
+// of bare []byte in case there is a need to extend this info later.
+type ChunkMap map[uint64][]*ChunkInfo
+
 type ChunkingFSM struct {
 	underlying raft.FSM
-	opMap      map[uint64][]chunkInfo
+	opMap      ChunkMap
+	lastTerm   uint64
 }
 
 type ChunkingConfigurationStore struct {
@@ -42,20 +34,22 @@ type ChunkingConfigurationStore struct {
 }
 
 func NewChunkingFSM(underlying raft.FSM) raft.FSM {
-	return &ChunkingFSM{
+	ret := &ChunkingFSM{
 		underlying: underlying,
-		opMap:      make(map[uint64][]chunkInfo),
+		opMap:      make(ChunkMap),
 	}
+	return ret
 }
 
 func NewChunkingConfigurationStore(underlying raft.ConfigurationStore) raft.ConfigurationStore {
-	return &ChunkingConfigurationStore{
+	ret := &ChunkingConfigurationStore{
 		ChunkingFSM: &ChunkingFSM{
 			underlying: underlying,
-			opMap:      make(map[uint64][]chunkInfo),
+			opMap:      make(ChunkMap),
 		},
 		underlyingConfigurationStore: underlying,
 	}
+	return ret
 }
 
 // Apply applies the log, handling chunking as needed. The return value will
@@ -66,74 +60,61 @@ func (c *ChunkingFSM) Apply(l *raft.Log) interface{} {
 		return c.underlying.Apply(l)
 	}
 
+	if l.Term != c.lastTerm {
+		// Term has changed. A raft library client that was applying chunks
+		// should get an error that it's no longer the leader and bail, and
+		// then any client of (Consul, Vault, etc.) should then retry the full
+		// chunking operation automatically, which will be under a different
+		// opnum. So it should be safe in this case to clear the map.
+		c.opMap = make(ChunkMap)
+		c.lastTerm = l.Term
+	}
+
+	// Get chunk info from extensions
 	var ci types.ChunkInfo
 	if err := proto.Unmarshal(l.Extensions, &ci); err != nil {
 		return errwrap.Wrapf("error unmarshaling chunk info: {{err}}", err)
 	}
-
 	opNum := ci.OpNum
 	seqNum := ci.SequenceNum
 
-	if opNum == 0 {
-		return ErrInvalidOpNum
-	}
-
-	// Look up existing chunks
+	// Look up existing chunks; if not existing, make placeholders in the slice
 	chunks, ok := c.opMap[opNum]
 	if !ok {
-		if seqNum != 0 {
-			return ErrNoExistingChunks
+		chunks = make([]*ChunkInfo, ci.NumChunks)
+		c.opMap[opNum] = chunks
+	}
+
+	// Insert the data
+	chunks[seqNum] = &ChunkInfo{Data: l.Data}
+
+	for _, chunk := range chunks {
+		// Check for nil, but also check data length in case it ends up
+		// unmarshaling weirdly for some reason where it makes a new struct
+		// instead of keeping the pointer nil
+		if chunk == nil || len(chunk.Data) == 0 {
+			// Not done yet, so return
+			return nil
 		}
 	}
 
-	// Do early detection of a loss or other problem
-	if int(seqNum) != len(chunks) {
-		delete(c.opMap, opNum)
-		return ErrSequenceNumberMismatch
+	finalData := make([]byte, 0, len(chunks)*raft.SuggestedMaxDataSize)
+
+	for _, chunk := range chunks {
+		finalData = append(finalData, chunk.Data...)
+	}
+	delete(c.opMap, opNum)
+
+	// Use the latest log's values with the final data
+	logToApply := &raft.Log{
+		Index:      l.Index,
+		Term:       l.Term,
+		Type:       l.Type,
+		Data:       finalData,
+		Extensions: ci.NextExtensions,
 	}
 
-	chunks = append(chunks, chunkInfo{
-		term:   l.Term,
-		seqNum: seqNum,
-		data:   l.Data,
-	})
-
-	if ci.SequenceNum == ci.NumChunks-1 {
-		// Run through and reconstruct the data
-		finalData := make([]byte, 0, len(chunks)*raft.SuggestedMaxDataSize)
-
-		var term uint64
-		for i, chunk := range chunks {
-			if i == 0 {
-				term = chunk.term
-			}
-			if chunk.seqNum != uint32(i) {
-				delete(c.opMap, opNum)
-				return ErrMissingChunk
-			}
-			if chunk.term != term {
-				delete(c.opMap, opNum)
-				return ErrTermMismatch
-			}
-			finalData = append(finalData, chunk.data...)
-		}
-
-		// Use the latest log's values with the final data
-		logToApply := &raft.Log{
-			Index:      l.Index,
-			Term:       l.Term,
-			Type:       l.Type,
-			Data:       finalData,
-			Extensions: ci.NextExtensions,
-		}
-
-		delete(c.opMap, opNum)
-		return c.Apply(logToApply)
-	}
-
-	// Otherwise, re-add to map and return
-	c.opMap[opNum] = chunks
-	return nil
+	return c.Apply(logToApply)
 }
 
 func (c *ChunkingFSM) Snapshot() (raft.FSMSnapshot, error) {
@@ -148,6 +129,14 @@ func (c *ChunkingFSM) Restore(rc io.ReadCloser) error {
 // if it's not used in client code
 func (c *ChunkingFSM) Underlying() raft.FSM {
 	return c.underlying
+}
+
+func (c *ChunkingFSM) CurrentState() (ChunkMap, error) {
+	snapMap, err := copystructure.Copy(c.opMap)
+	if err != nil {
+		return nil, err
+	}
+	return snapMap.(ChunkMap), nil
 }
 
 func (c *ChunkingConfigurationStore) StoreConfiguration(index uint64, configuration raft.Configuration) {
--- fsm_test.go
+++ fsm_test.go
@@ -5,10 +5,7 @@ import (
 	"testing"
 
 	"github.com/go-test/deep"
-	proto "github.com/golang/protobuf/proto"
-	"github.com/hashicorp/go-raftchunking/types"
 	"github.com/hashicorp/raft"
-	"github.com/kr/pretty"
 )
 
 type MockFSM struct {
@@ -65,219 +62,74 @@ func TestFSM_Basic(t *testing.T) {
 	}
 }
 
-func TestFSM_ErrorConditions(t *testing.T) {
+func TestFSM_CurrentState(t *testing.T) {
+	m := new(MockFSM)
+	f := NewChunkingFSM(m).(*ChunkingFSM)
+
 	data, logs := chunkData(t)
 
-	var m *MockFSM
-	var f *ChunkingFSM
-	var err error
-
-	// OpNum of zero
-	{
-		m = new(MockFSM)
-		f = NewChunkingFSM(m).(*ChunkingFSM)
-
-		old := logs[1].Extensions
-		var ci types.ChunkInfo
-		if err := proto.Unmarshal(logs[1].Extensions, &ci); err != nil {
-			t.Fatal(err)
+	for i, l := range logs {
+		if i == len(logs)-1 {
+			break
 		}
-		ci.OpNum = 0
-		if logs[1].Extensions, err = proto.Marshal(&ci); err != nil {
-			t.Fatal(err)
-		}
-
-		var done bool
-		for i, l := range logs {
-			r := f.Apply(&l)
-			switch r.(type) {
-			case nil:
-				if i == 1 {
-					t.Fatal("expected bad op ID error")
-				}
-			case error:
-				err := r.(error)
-				if i != 1 {
-					t.Fatalf("unexpected error at value %d: %v; chunk info is %s", i, err, pretty.Sprint(ci))
-				}
-				if err != ErrInvalidOpNum {
-					t.Fatalf("unexpected error: %v", err)
-				}
-				// Success
-				done = true
-			default:
-				t.Fatal("unexpected return value")
-			}
-
-			if done {
-				break
-			}
-		}
-
-		logs[1].Extensions = old
-	}
-
-	// Invalid sequence number
-	{
-		m = new(MockFSM)
-		f = NewChunkingFSM(m).(*ChunkingFSM)
-
-		old := logs[0].Extensions
-		var ci types.ChunkInfo
-		if err := proto.Unmarshal(logs[0].Extensions, &ci); err != nil {
-			t.Fatal(err)
-		}
-		ci.SequenceNum = 1
-		if logs[0].Extensions, err = proto.Marshal(&ci); err != nil {
-			t.Fatal(err)
-		}
-
-		r := f.Apply(&(logs[0]))
-		if r == nil {
-			t.Fatal("expected error")
-		}
-		if r.(error) != ErrNoExistingChunks {
+		r := f.Apply(&l)
+		switch r.(type) {
+		case nil:
+		case error:
 			t.Fatal(r.(error))
+		case int:
+			if i != len(logs)-1 {
+				t.Fatal("got int back before apply should have happened")
+			}
+			if r.(int) != 1 {
+				t.Fatalf("unexpected number of logs back: %d", r.(int))
+			}
+		default:
+			t.Fatal("unexpected return value")
 		}
-
-		logs[0].Extensions = old
 	}
 
-	// Mismatched sequence number, greater than or less than number of chunks
-	{
-		seqNumReplacement := func(seqNum uint32) {
-			m = new(MockFSM)
-			f = NewChunkingFSM(m).(*ChunkingFSM)
-
-			old := logs[1].Extensions
-			var ci types.ChunkInfo
-			if err := proto.Unmarshal(logs[1].Extensions, &ci); err != nil {
-				t.Fatal(err)
-			}
-			ci.SequenceNum = seqNum
-			if logs[1].Extensions, err = proto.Marshal(&ci); err != nil {
-				t.Fatal(err)
-			}
-
-			var done bool
-			for i, l := range logs {
-				r := f.Apply(&l)
-				switch r.(type) {
-				case nil:
-					if i == 1 {
-						t.Fatal("expected missing seqnum error")
-					}
-				case error:
-					err := r.(error)
-					if i != 1 {
-						t.Fatalf("unexpected error at value %d: %v; chunk info is %s", i, err, pretty.Sprint(ci))
-					}
-					if err != ErrSequenceNumberMismatch {
-						t.Fatalf("unexpected error: %v", err)
-					}
-					// Success
-					done = true
-				default:
-					t.Fatal("unexpected return value")
-				}
-
-				if done {
-					break
-				}
-			}
-
-			logs[1].Extensions = old
+	var opCount int
+	for _, v := range f.opMap {
+		opCount++
+		if opCount > 1 {
+			t.Fatalf("unexpected opcount: %d", opCount)
 		}
-
-		seqNumReplacement(3)
-		seqNumReplacement(0)
+		var validChunks int
+		for _, val := range v {
+			if val != nil {
+				validChunks++
+			}
+		}
+		if validChunks != len(logs)-1 {
+			t.Fatalf("unexpected number of chunks: %d; should be one less than len of logs which is %d", validChunks, len(logs))
+		}
 	}
 
-	// Term changes
-	{
-		m = new(MockFSM)
-		f = NewChunkingFSM(m).(*ChunkingFSM)
-
-		old := logs[1].Term
-		logs[1].Term = 5
-
-		var done bool
-		for i, l := range logs {
-			r := f.Apply(&l)
-			switch r.(type) {
-			case nil:
-				if i == len(logs)-1 {
-					t.Fatal("expected term mismatch")
-				}
-			case error:
-				err := r.(error)
-				if i != len(logs)-1 {
-					t.Fatalf("unexpected error at value %d: %v", i, err)
-				}
-				if err != ErrTermMismatch {
-					t.Fatalf("unexpected error: %v", err)
-				}
-				// Success
-				done = true
-			default:
-				t.Fatal("unexpected return value")
-			}
-
-			if done {
-				break
-			}
-		}
-
-		logs[1].Term = old
+	currState, err := f.CurrentState()
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	// Mismatched data (NumChunks is incorrect)
-	{
-		m = new(MockFSM)
-		f = NewChunkingFSM(m).(*ChunkingFSM)
+	if diff := deep.Equal(f.opMap, currState); diff != nil {
+		t.Fatal(diff)
+	}
 
-		old := logs[7].Extensions
-		var ci types.ChunkInfo
-		if err := proto.Unmarshal(logs[7].Extensions, &ci); err != nil {
-			t.Fatal(err)
-		}
-		ci.NumChunks = 8
-		if logs[7].Extensions, err = proto.Marshal(&ci); err != nil {
-			t.Fatal(err)
-		}
+	r := f.Apply(&(logs[len(logs)-1]))
+	rInt, ok := r.(int)
+	if !ok {
+		t.Fatalf("wrong type back: %T, value is %#v", r, r)
+	}
+	if rInt != 1 {
+		t.Fatalf("unexpected number of logs back: %d", rInt)
+	}
 
-		var done bool
-		for i, l := range logs {
-			r := f.Apply(&l)
-			switch r.(type) {
-			case nil:
-				if i == len(logs)-1 {
-					t.Fatal("expected non-nil value for last log apply")
-				}
-			case error:
-				t.Fatal(r.(error))
-			case int:
-				if i != 7 {
-					t.Fatal("got int back before apply should have happened")
-				}
-				done = true
-			default:
-				t.Fatal("unexpected return value")
-			}
-			if done {
-				break
-			}
-		}
+	var finalData []byte
+	for _, l := range m.logs {
+		finalData = append(finalData, l...)
+	}
 
-		var finalData []byte
-		for _, l := range m.logs {
-			finalData = append(finalData, l...)
-		}
-
-		if diff := deep.Equal(data, finalData); diff == nil {
-			t.Fatal("expected a difference")
-		}
-
-		logs[7].Extensions = old
+	if diff := deep.Equal(data, finalData); diff != nil {
+		t.Fatal(diff)
 	}
 }
